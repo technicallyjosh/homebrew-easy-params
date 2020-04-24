6556d685fca61a14192b22ee05c78fc6d433e615
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-24 13:49:42 -0600

    Brew formula update for easy-params version v0.4.3

--- easy-params.rb
+++ easy-params.rb
@@ -2,16 +2,16 @@
 class EasyParams < Formula
   desc "Opinionated CLI for easy management of SSM Parameters."
   homepage "https://github.com/technicallyjosh/easy-params"
-  version "0.4.2"
+  version "0.4.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.4.2/easy-params_0.4.2_Darwin_x86_64.tar.gz"
-    sha256 "76036ef1e21dbde7312a916e920801b6f746e34d92a9355ed35e70c939d9c6d2"
+    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.4.3/easy-params_0.4.3_Darwin_x86_64.tar.gz"
+    sha256 "cdcfe273e1107c09bde7cc3cd3654aa247f58200e0aeb60c1d6a74bf265230b1"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/technicallyjosh/easy-params/releases/download/v0.4.2/easy-params_0.4.2_Linux_x86_64.tar.gz"
-      sha256 "4d57ceae9fea6b895c46e678c6d10b0346f76bdc14b660c80ef4d8b4c58b560e"
+      url "https://github.com/technicallyjosh/easy-params/releases/download/v0.4.3/easy-params_0.4.3_Linux_x86_64.tar.gz"
+      sha256 "d9320f7140dbe4497f2e3db2c60d1034889f6576d5693e3740a59836340fd92d"
     end
   end
 
