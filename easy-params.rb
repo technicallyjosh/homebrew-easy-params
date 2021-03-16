02901eb67af4ea8243c1ce1aeb73813e3b5b5508
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-16 16:30:25 -0600

    Brew formula update for easy-params version v0.6.2

--- easy-params.rb
+++ easy-params.rb
@@ -5,24 +5,24 @@
 class EasyParams < Formula
   desc "Opinionated CLI for easy management of SSM Parameters."
   homepage "https://github.com/technicallyjosh/easy-params"
-  version "0.6.1"
+  version "0.6.2"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.6.1/easy-params_0.6.1_Darwin_x86_64.tar.gz"
-    sha256 "348804a3e2032c52c8e11933f1cfb45bcf525566ee4f1cc7ef8cde2992806288"
+    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.6.2/easy-params_0.6.2_Darwin_x86_64.tar.gz"
+    sha256 "ecb12d57b6145149d9109b46cb04003b562b20570fd56ff1b6165c6b4a2eef41"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.6.1/easy-params_0.6.1_Darwin_arm64.tar.gz"
-    sha256 "a3bd9006b7203d1206cc41b977b4496f020103a69ff472c5f491b7666c0bb438"
+    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.6.2/easy-params_0.6.2_Darwin_arm64.tar.gz"
+    sha256 "d70d64c8c6feacc27da20ff5453313c7371060a5eb9d6d1114958394edc23baa"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.6.1/easy-params_0.6.1_Linux_x86_64.tar.gz"
-    sha256 "daa4229ec4e1c0092e161c4a2804cbdf4cbe22611edec11c1d0b990530b3733f"
+    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.6.2/easy-params_0.6.2_Linux_x86_64.tar.gz"
+    sha256 "d86ca5cbf6976ed53cf9df770067260792ab7f7a53ec772951f5141fc1b040bd"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.6.1/easy-params_0.6.1_Linux_arm64.tar.gz"
-    sha256 "ba396dbe6755b527a3b121d9fd5a9128820fc3f1c3714dad05af39c773dd649c"
+    url "https://github.com/technicallyjosh/easy-params/releases/download/v0.6.2/easy-params_0.6.2_Linux_arm64.tar.gz"
+    sha256 "6d9d86004aec59c1c927990a4faf1cb32517e93e28a08dbcda6d87c4637d8c87"
   end
 
   def install
